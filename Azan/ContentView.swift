67bb62e931b397691a833e665994a77256311188
Faiz Mokhtar <mfmokhtar@gmail.com>
2020-04-18 21:58:06 +0800

    design basic UI

--- Azan/ContentView.swift
+++ Azan/ContentView.swift
@@ -10,7 +10,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Spacer()
+                    Text("15 minutes left until Maghrib")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    HStack{
+                        Image(systemName: "mappin.circle.fill")
+                        Text("Kuala Lumpur, Malaysia")
+                            .fontWeight(.medium)
+                    }
+                    .font(.headline)
+                }
+                .frame(height: 200, alignment: .leading)
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Subuh")
+                        Spacer()
+                        Text("5:50 AM")
+                        Image(systemName: "bell.fill")
+                    }
+                    HStack {
+                        Text("Zuhur")
+                        Spacer()
+                        Text("1:20 PM")
+                        Image(systemName: "bell.slash.fill")
+                    }
+                    HStack {
+                        Text("Asar")
+                        Spacer()
+                        Text("4:30 PM")
+                        Image(systemName: "bell.slash.fill")
+                    }
+                    HStack {
+                        Text("Maghrib")
+                        Spacer()
+                        Text("7:30 PM")
+                        Image(systemName: "bell.slash.fill")
+                    }
+                    HStack {
+                        Text("Isyak")
+                        Spacer()
+                        Text("8:30 PM")
+                        Image(systemName: "bell.slash.fill")
+                    }
+                }
+                .padding(.horizontal, 50.0)
+                .padding(.top, 20)
+            }
+            .navigationBarTitle("Today")
+        }
     }
 }
 
