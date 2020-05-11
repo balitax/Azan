c2521c65a840238b73583c2a3c6cdfb2b1827636
Faiz Mokhtar <mfmokhtar@gmail.com>
2020-05-11 13:48:40 +0800

    Include icon

--- Azan/SolatView.swift
+++ Azan/SolatView.swift
@@ -1,25 +1,22 @@
-//
-//  SolatView.swift
-//  Azan
-//
-//  Created by Faiz Mokhtar on 19/04/2020.
-//  Copyright © 2020 Faiz Mokhtar. All rights reserved.
-//
-
 import SwiftUI
 
 struct SolatView: View {
 
     let title: String
     let time: String
+    let icon: String
 
-    init(title: String, time: String) {
+    init(title: String, time: String, icon: String = "") {
         self.title = title
         self.time = time
+        self.icon = icon
     }
 
     var body: some View {
         HStack {
+            if !icon.isEmpty {
+                Image(systemName: icon)
+            }
             Text(title.uppercased())
                 .fontWeight(.semibold)
             Spacer()
@@ -32,7 +29,7 @@ struct SolatView: View {
 
 struct SolatView_Previews: PreviewProvider {
     static var previews: some View {
-        SolatView(title: "Zuhur", time: "8:50 AM")
+        SolatView(title: "Zuhur", time: "8:50 AM", icon: "icon.max.fill")
             .previewLayout(.sizeThatFits)
     }
 }
