4c582102aec88b907a22e5e47905ad4568df7c05
Faiz Mokhtar <mfmokhtar@gmail.com>
2020-05-20 00:37:48 +0800

    add boostrap file

---
+++ boostrap.sh
@@ -0,0 +1,9 @@
+# Install gems if a Gemfile exists
+if [ -e "Gemfile" ]; then
+  echo "installing ruby gems";
+
+  # install bundler gem for ruby dependency management
+  gem install bundler --no-document || echo "failed to install bundle";
+  bundle config set deployment 'true';
+  bundle install || echo "failed to install bundle";
+fi
